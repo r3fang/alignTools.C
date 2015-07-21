a6cfdacc31cba410597cb00bebc80c4fbb8541b7
Rongxin Fang <r3fang@ucsd.edu>
2015-07-21 11:02:36 -0700

    report global alignment score

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
-all:src/align.c src/utils.c src/utils.h src/kseq.h
-		$(CC) -g -O2 src/align.c src/utils.c -o bin/align -lz
+all:src/align.c src/utils.c src/utils.h src/kseq.h src/kstring.c src/kstring.h
+		$(CC) -g -O2 src/align.c src/utils.c src/kstring.c -o bin/align -lz
 
 clean:
 		rm -f bin/*.dSYM
-		rm -r bin/*.dSYM
--- src/align.c
+++ src/align.c
@@ -11,9 +11,9 @@
 #include <string.h>
 #include "utils.h"
 #include "kseq.h"
+#include "kstring.h"
 
 KSEQ_INIT(gzFile, gzread);
-
 
 #define GL_ERR_NONE 			0
 #define GAP 					-1.0
@@ -23,47 +23,30 @@ KSEQ_INIT(gzFile, gzread);
 typedef enum {true, false} bool;
 
 typedef struct {
-  char *seq1;
-  unsigned int len1;
-  char *seq2;
-  unsigned int len2;
-} seq_pair;
-typedef seq_pair *seq_pair_t;
-
-typedef struct {
   unsigned int m;
   unsigned int n;
-  double **mat;
-} matrix;
-typedef matrix *matrix_t;
+  double **score;
+} matrix_t;
 
-matrix_t create_matrix(size_t m, size_t n){
+matrix_t *create_matrix(size_t m, size_t n){
 	size_t i, j; 
-	matrix_t S = mycalloc(1, matrix);
+	matrix_t *S = mycalloc(1, matrix_t);
 	S->m = m;
 	S->n = n;
-	S->mat = mycalloc(m, double*);
+	S->score = mycalloc(m, double*);
 	for (i = 0; i < m; i++) {
-      S->mat[i] = mycalloc(n, double);
+      S->score[i] = mycalloc(n, double);
     }
 	return S;
 }
 
-int destory_matrix(matrix_t S){
+int destory_matrix(matrix_t *S){
 	if(S == NULL) die("destory_matrix: parameter error\n");
 	int i, j;
 	for(i = 0; i < S->m; i++){
-		free(S->mat[i]);
+		free(S->score[i]);
 	}
 	free(S);
-	return GL_ERR_NONE;
-}
-
-int destory_seq_pair(seq_pair_t p){
-	if(p == NULL) die("destory_seq_pair: parameter error\n");
-	free(p->seq1);
-	free(p->seq2);
-	free(p);
 	return GL_ERR_NONE;
 }
 
@@ -75,36 +58,24 @@ double max3(double a1, double a2, double a3){
 	return res;
 }
 
-double global(seq_pair *problem, bool local){
-	size_t m   = problem->len1 + 1;
-	size_t n   = problem->len2 + 1;
-	matrix_t S = create_matrix(m, n);
+double global(kstring_t *s1, kstring_t *s2){
+	size_t m   = s1->l + 1;
+	size_t n   = s2->l + 1;
+	matrix_t *S = create_matrix(m, n);
 	size_t i, j, k, l;
-	for(i=0; i < S->m; i++) S->mat[i][0] = 0.0;
-	for(j=0; j < S->n; j++) S->mat[0][j] = 0.0;
+	for(i=0; i < S->m; i++) S->score[i][0] = 0.0;
+	for(j=0; j < S->n; j++) S->score[0][j] = 0.0;
 	
-	for(i = 1; i <= problem->len1; i++){
-		for(j = 1; j <= problem->len2; j++){
-	        double new_score = (strncmp(problem->seq1+(i-1), problem->seq2+(j-1), 1) == 0) ? MATCH : MISMATCH;
-			S->mat[i][j] = DBL_MIN;
-			S->mat[i][j] = max3(S->mat[i-1][j-1] + new_score, S->mat[i-1][j] + GAP, S->mat[i][j-1] + GAP);
+	for(i = 1; i <= s1->l; i++){
+		for(j = 1; j <= s2->l; j++){
+	        double new_score = (strncmp(s1->s+(i-1), s2->s+(j-1), 1) == 0) ? MATCH : MISMATCH;
+			S->score[i][j] = DBL_MIN;
+			S->score[i][j] = max3(S->score[i-1][j-1] + new_score, S->score[i-1][j] + GAP, S->score[i][j-1] + GAP);
 		}
 	}
-	double res = S->mat[problem->len1][problem->len2];
+	double res = S->score[s1->l][s2->l];
 	if(destory_matrix(S) != GL_ERR_NONE) die("smith_waterman: fail to destory matrix");
 	return res;
-}
-
-seq_pair_t init_pair_t(char* str1, char* str2){
-	if(str1 == NULL || str2 == NULL) die("init_pair_t: parameter error\n");
-    seq_pair_t problem = mycalloc(1, seq_pair);
-	problem->seq1 = mycalloc(strlen(str1), char);
-	problem->seq2 = mycalloc(strlen(str2), char);
-	problem->seq1 = strdup(str1);
-	problem->len1 = strlen(problem->seq1);
-	problem->seq2 = strdup(str2);
-	problem->len2 = strlen(problem->seq2);
-	return problem;
 }
 
 char* str_toupper(char* s){
@@ -120,30 +91,45 @@ char* str_toupper(char* s){
 	return r;
 }
 
-/* main function. */
-int main(int argc, char *argv[]) {	
+void read_kstring(char* fname, kstring_t *str1, kstring_t *str2){
 	gzFile fp;
 	kseq_t *seq;
-	char *str1, *str2; 
-	int l;
+	fp = gzopen(fname, "r");
+	seq = kseq_init(fp);
+	int i, l;
+	char **tmp = mycalloc(3, char*);
+	i = 0;
+	while((l=kseq_read(seq)) >= 0){
+		if(i >= 2) die("input fasta file has more than 2 sequences");
+		tmp[i++] = str_toupper(seq->seq.s);
+	}
+	if(tmp[0] == NULL || tmp[1] == NULL) die("read_kstring: fail to read sequence");
+	(str1)->s = strdup(tmp[0]); (str1)->l = strlen((str1)->s);
+	(str2)->s = strdup(tmp[1]); (str2)->l = strlen((str2)->s);
+	for(; i >=0; i--){if(tmp[i]) free(tmp[i]);} free(tmp);
+	kseq_destroy(seq);
+	gzclose(fp);
+}
+
+void kstring_destory(kstring_t *ks){
+	free(ks->s);
+	free(ks);
+}
+
+/* main function. */
+int main(int argc, char *argv[]) {	
+	kstring_t *ks1, *ks2; 
+	ks1 = mycalloc(1, kstring_t);
+	ks2 = mycalloc(1, kstring_t);
+	
 	if (argc == 1) {
 		fprintf(stderr, "Usage: %s <in.seq>\n", argv[0]);
 		return 1;
 	}
-	fp = gzopen(argv[1], "r");
-	seq = kseq_init(fp);
-	if((l=kseq_read(seq)) >= 0){
-		str1 = str_toupper(seq->seq.s);
-	}
-	if((l=kseq_read(seq) >= 0)){
-		str2 = str_toupper(seq->seq.s);
-	}
-	if(str1 == NULL || str2 == NULL) die("fail to read sequence\n");
-    printf("%s\t%s\n", str1, str2);
-	seq_pair_t problem = init_pair_t(str1, str2);
-	printf("score=%f\n", global(problem, false));
-	if(destory_seq_pair(problem) != GL_ERR_NONE) die("destory_seq_pair fails\n");
-	kseq_destroy(seq);
-	gzclose(fp);
+	read_kstring(argv[1], ks1, ks2);
+	if(ks1->s == NULL || ks2->s == NULL) die("fail to read sequence\n");
+	printf("score=%f\n", global(ks1, ks2));
+	kstring_destory(ks1);
+	kstring_destory(ks2);
 	return 0;
 }
