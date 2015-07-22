f8b5694719149f1968e2f92734e6972d5beb9b21
Rongxin Fang <r3fang@ucsd.edu>
2015-07-22 14:39:33 -0700

    update Makefile and fix bug in global.c

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-all:src/global_affine_gap.c src/local.c	src/utils.c src/utils.h src/kseq.h src/kstring.c src/kstring.h
-		$(CC) -g -O2 src/global_affine_gap.c src/utils.c src/kstring.c -o bin/align -lz
+all:src/global_affine_gap.c src/global.c src/local.c	src/utils.c src/utils.h src/kseq.h src/kstring.c src/kstring.h
+		$(CC) -g -O2 src/global_affine_gap.c src/utils.c src/kstring.c -o bin/global_affine_gap -lz
 		$(CC) -g -O2 src/local.c src/utils.c src/kstring.c -o bin/local -lz
-
+		$(CC) -g -O2 src/global.c src/utils.c src/kstring.c -o bin/global -lz
 clean:
 		rm -f bin/*.dSYM
--- src/global.c
+++ src/global.c
@@ -2,7 +2,18 @@
 /* global.c 		                                                  */
 /* Author: Rongxin Fang                                               */
 /* E-mail: r3fang@ucsd.edu                                            */
+/* Date: 07-22-2015                                                   */
 /* Pair wise global alignment without affine gap.                     */
+/* initilize S(i,j):                                                  */
+/* S(i, 0) = g*(i); S(0, j) = g*(j)                                   */
+/* reccurrance relations:                                             */
+/* S(i,j) = max{S(i-1, j-1)+s(x,y), S(i-1, j)+gap, S(i, j-1)+gap}     */
+/*               (S(i-1, j-1)+s(x,y))   # DIAGONAL                    */
+/* S(i,j) = max  (   S(i-1, j)+gap  )   # RIGHT                       */
+/*               (   S(i, j-1)+gap  )   # LEFT                        */
+/* Traceback:                                                         */
+/* S(m, n) holds the optimal alignment score; trace pointers back     */
+/* from S(m, n) to S(0, 0) to recover alignment.                      */
 /*--------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -49,6 +60,13 @@ matrix_t *create_matrix(size_t m, size_t n){
 	for (i = 0; i < m; i++) {
       S->pointer[i] = mycalloc(n, int);
     }		
+	// initlize the first row and column of S
+	for(i=0; i < S->m; i++) S->score[i][0] = GAP*i;
+	for(j=0; j < S->n; j++) S->score[0][j] = GAP*j;	
+	
+	for(i=1; i < S->m; i++) S->pointer[i][0] = RIGHT;
+	for(j=1; j < S->n; j++) S->pointer[0][j] = LEFT;
+	
 	return S;
 }
 
@@ -88,11 +106,9 @@ int max3(double *res, double a1, double a2, double a3){
 	return state;
 }
 
-void trace_back(matrix_t *S, kstring_t *ks1, kstring_t *ks2, kstring_t *res_ks1, kstring_t *res_ks2){
+void trace_back(matrix_t *S, kstring_t *ks1, kstring_t *ks2, kstring_t *res_ks1, kstring_t *res_ks2, int i, int j){
 	if(S == NULL || ks1 == NULL || ks2 == NULL || res_ks1 == NULL || res_ks2 == NULL) die("trace_back: parameter error");
 	int m = 0; 
-	int i = ks1->l;
-	int j = ks2->l;
 	while(i>0 || j >0){
 		switch(S->pointer[i][j]){
 			case LEFT:
@@ -117,17 +133,12 @@ void trace_back(matrix_t *S, kstring_t *ks1, kstring_t *ks2, kstring_t *res_ks1,
 	res_ks2->s = strrev(res_ks2->s);
 }
 
-double global(kstring_t *s1, kstring_t *s2, kstring_t *r1, kstring_t *r2){
+double align(kstring_t *s1, kstring_t *s2, kstring_t *r1, kstring_t *r2){
 	if(s1 == NULL || s2 == NULL || r1 == NULL || r2 == NULL) die("global: parameter error\n");
 	size_t m   = s1->l + 1;
 	size_t n   = s2->l + 1;
 	matrix_t *S = create_matrix(m, n);
 	size_t i, j, k, l;
-	for(i=0; i < S->m; i++) S->score[i][0] = 0.0;
-	for(j=0; j < S->n; j++) S->score[0][j] = 0.0;
-
-	for(i=0; i < S->m; i++) S->pointer[i][0] = RIGHT;
-	for(j=0; j < S->n; j++) S->pointer[0][j] = LEFT;
 	
 	for(i = 1; i <= s1->l; i++){
 		for(j = 1; j <= s2->l; j++){
@@ -135,10 +146,10 @@ double global(kstring_t *s1, kstring_t *s2, kstring_t *r1, kstring_t *r2){
 			S->pointer[i][j] = max3(&S->score[i][j], S->score[i][j-1] + GAP, S->score[i-1][j-1] + new_score, S->score[i-1][j] + GAP);
 		}
 	}
-	double res = S->score[s1->l][s2->l];
-	trace_back(S, s1, s2, r1, r2);
+	// start tracing back from (m,n)
+	trace_back(S, s1, s2, r1, r2, s1->l, s2->l);
 	if(destory_matrix(S) != GL_ERR_NONE) die("smith_waterman: fail to destory matrix");
-	return res;
+	return S->score[s1->l][s2->l];
 }
 
 char* str_toupper(char* s){
@@ -194,7 +205,7 @@ int main(int argc, char *argv[]) {
 	kstring_t *r2 = mycalloc(1, kstring_t);
 	r1->s = mycalloc(ks1->l + ks2->l, char);
 	r2->s = mycalloc(ks1->l + ks2->l, char);
-	printf("score=%f\n", global(ks1, ks2, r1, r2));
+	printf("score=%f\n", align(ks1, ks2, r1, r2));
 	printf("%s\n%s\n", r1->s, r2->s);
 	kstring_destory(ks1);
 	kstring_destory(ks2);
